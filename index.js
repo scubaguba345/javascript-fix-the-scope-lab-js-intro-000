556084934d932c45b98144d307abc2627354efef
brandon gubitosa <brandon.gubitosa@gmail.com>
2020-02-08 13:41:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ function myAnimal() {
 }
 
 function yourAnimal() {
+
+  var animal = 'cat'
   // The tests expect this function to return `animal` just like the previous function
   // However, you cannot simply modify the existing variable declared on line 1 in the global scope
   // How can we make sure that this function
@@ -17,7 +19,6 @@ function yourAnimal() {
 function add2(n) {
   return n + 2
 
-  // Feel free to move things around!
   const two = 2
 }
 
